d42940e1ebcc392161562d0cd61b2fa0fadfe026
Subhitha G S <subhitharaam@gmail.com>
2026-07-09 16:23:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var noOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfButtons; i++) 
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", sound);
+}
+
+function sound() 
+{
+    var buttoninnerhtml=this.innerHTML;
+    switch(buttoninnerhtml)
+    {
+        case 'w':
+            var tom1=new audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
+        case 'a':
+            var tom2=new audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case 's':
+            var tom3=new audio("sounds/tom-3.mp3");
+             tom3.play();
+            break;
+        case 'd':
+            var tom4=new audio("sounds/tom-4.mp3");
+             tom4.play();
+            break;
+        case 'j':
+            var crash=new audio("sounds/crash.mp3");
+             crash.play();
+            break;
+        case 'k':
+            var snare=new audio("sounds/snare.mp3");
+             snare.play();
+            break;
+        case 'l':
+            var kick=new audio("sounds/kick.mp3");
+             kick.play();
+            break;
+        default:
+            console.log("buttoninnerhtml");
+    }
+
+}
