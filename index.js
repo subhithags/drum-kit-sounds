fe3f208a89ee919edbd06816d4824cfc1a569906
Subhitha G S <subhitharaam@gmail.com>
2026-07-09 16:24:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,31 +11,31 @@ function sound()
     switch(buttoninnerhtml)
     {
         case 'w':
-            var tom1=new audio("sounds/tom-1.mp3")
+            var tom1=new audio("tom-1.mp3")
             tom1.play();
             break;
         case 'a':
-            var tom2=new audio("sounds/tom-2.mp3");
+            var tom2=new audio("tom-2.mp3");
             tom2.play();
             break;
         case 's':
-            var tom3=new audio("sounds/tom-3.mp3");
+            var tom3=new audio("tom-3.mp3");
              tom3.play();
             break;
         case 'd':
-            var tom4=new audio("sounds/tom-4.mp3");
+            var tom4=new audio("tom-4.mp3");
              tom4.play();
             break;
         case 'j':
-            var crash=new audio("sounds/crash.mp3");
+            var crash=new audio("crash.mp3");
              crash.play();
             break;
         case 'k':
-            var snare=new audio("sounds/snare.mp3");
+            var snare=new audio("snare.mp3");
              snare.play();
             break;
         case 'l':
-            var kick=new audio("sounds/kick.mp3");
+            var kick=new audio("kick.mp3");
              kick.play();
             break;
         default:
